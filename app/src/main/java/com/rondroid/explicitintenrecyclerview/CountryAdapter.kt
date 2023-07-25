cc7e34a820487d76e615aadacc29acb9406ceeba
Ronnie <ronald.otiego@gmail.com>
2023-07-25 15:39:22 +0300

    add adapter class

--- app/src/main/java/com/rondroid/explicitintenrecyclerview/CountryAdapter.kt
+++ app/src/main/java/com/rondroid/explicitintenrecyclerview/CountryAdapter.kt
@@ -1,4 +1,33 @@
 package com.rondroid.explicitintenrecyclerview
 
-class CountryAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.countryitem_layout.view.*
+
+class CountryAdapter(val countryArrayList:ArrayList<CountryData>, val context: Context):RecyclerView.Adapter<CountryAdapter.CountryViewHolder>(){
+
+    inner class CountryViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+
+        fun bindItems(countryData: CountryData){
+            itemView.item_countryTv.text = countryData.country
+            itemView.item_countryCapitalTv.text = countryData.capital
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.countryitem_layout,parent,false)
+        return CountryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        holder.bindItems(countryArrayList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return countryArrayList.size
+    }
 }
